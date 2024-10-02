b0e7895faed5d7e0df600b9b8ccb2cea2fc5b4c5
Mrezadwiprasetiawan <Mrezadwiprasetiawan@gmail.com>
2024-10-02 16:04:39 +0700

    release resources

---
+++ ByteCode/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+  java
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
+}
+
+val currentDir = projectDir.absolutePath
+val sourcesDir = file("$currentDir/sources")
+val javaDir = file("$currentDir/src/main/java/id/pras/jvmlang/bytecode")
+val generatorDir = file("$projectDir/build/generator")
+
+tasks.register<Copy>("prepareGeneratorDir") {
+  from(generatorDir)
+  into(generatorDir)
+}
+
+tasks.register<Exec>("CompileGen") {
+  doFirst {
+    println("Compiling Generator class")
+    commandLine(
+      "${System.getenv("JAVA_HOME")}/bin/javac",
+      "-d",
+      generatorDir.absolutePath,
+      "$javaDir/Gen.java"
+    )
+    standardOutput = System.out
+    errorOutput = System.err
+  }
+  doLast {
+    println("Compile success")
+  }
+}
+
+var runGeneratorTasks = ""
+sourcesDir.listFiles()?.forEachIndexed { index, file ->
+  val taskName = "runGenerator"
+  tasks.register("$taskName$index", JavaExec::class.java) {
+    classpath = files(generatorDir)
+    mainClass.set("id.pras.jvmlang.bytecode.Gen")
+    args = listOf(
+      "-i",
+      file.absolutePath,
+      "-o",
+      "$projectDir/src/main/java/id/pras/jvmlang/bytecode/${file.nameWithoutExtension}.java",
+      "-p",
+      "id.pras.jvmlang.bytecode"
+    )
+    if(index==0) dependsOn("CompileGen") else dependsOn("$taskName${index-1}")
+  }
+  runGeneratorTasks=taskName
+}
+
+tasks.register("GenJavaFiles"){
+  dependsOn(runGeneratorTasks)
+  doFirst{
+    println("starting Generator")
+  }
+  doLast{
+    println("generated successfully")
+  }
+}
